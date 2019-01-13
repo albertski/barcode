8e5317fc852a766f81aeba51b0523f5cee1a415a
Albert Jankowski <albert@xenomedia.com>
2019-01-12 20:09:45 -0600

    Adds web support.

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+import HybridApp from './src/App';
+const App = (props) => {
+  return (
+    <HybridApp />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
